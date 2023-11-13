ee5055334054661452cf46034c463d700e78a0d9
mooncy0809 <mooncy0809@naver.com>
2023-11-14 00:06:44 +0900

    Setting: 초기환경세팅

---
+++ src/main/java/com/wanted/budgetmgr/BudgetMgrApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.budgetmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetMgrApplication.class, args);
+	}
+
+}
